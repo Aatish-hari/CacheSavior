677364c6f12a5a17fc494befcec82885f7c54b58
Aatish-hari <aatishhari2006@gmail.com>
2026-07-10 02:07:36 +0000

    Error handling

--- include/functions.c
+++ include/functions.c
@@ -1,5 +1,16 @@
 #include"functions.h"
 
+struct element_inside_cache{
+    char* data;
+    int len;
+    char* url;
+    int number_of_usage;   //number of time that cache element is being used, more number = more priority
+    struct element_inside_cache *next;
+};
+
+struct element_inside_cache* head;  //pointer to starting of cache
+
+
 int connectRemoteServer(char* host_addrs, int server_port){
 
     int remoteSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -88,14 +99,84 @@ int handle_request(int client_socket, struct ParsedRequest *request, char* tempr
     int temp_buff_index = 0;
 
     while(bytes_recv>0){
-        bytes_recv = send(client_socket, buff, bytes_recv, 0);
-        for(int i = 0 ; i<bytes_recv/sizeof(char) ; i++){
+        bytes_send = send(client_socket, buff, bytes_send, 0);
+        for(int i = 0 ; i<bytes_send/sizeof(char) ; i++){
             temp_buff[temp_buff_index] = buff[i];
             temp_buff_index++;
         }
         temp_buff_size +=MAX_BYTES;
         temp_buff = (char*)realloc(temp_buff, temp_buff_size);
+        if(bytes_recv<0){
+            perror("Error: error sending data to client\n");
+            break;
+        }
+        bzero(buff, MAX_BYTES);
+        bytes_recv = recv(remoteServersocket, buff, MAX_BYTES-1, 0);
     }
-    
-    
+    free(buff);
+    temp_buff[temp_buff_index] = '\0';
+    adding_element_in_cache(temp_buff, strlen(temp_buff), tempreq);
+    free(temp_buff);
+    close(remoteServersocket);
+    return 0;
+}
+
+int checkHTTPversion(char* version){
+    int ans = -1;
+    if(strncmp(version, "HTTP/1.1", 8) == 0 || strncmp(version, "HTTP/1.0", 8) == 0){
+        ans = 1;
+    }
+    else{
+        ans = -1;
+    }
+    return ans;
+}
+
+
+
+int ErrorMessage(int socket, int status_code)
+{
+	char str[1024];
+	char currentTime[50];
+	time_t now = time(0);
+
+	struct tm data = *gmtime(&now);
+	strftime(currentTime,sizeof(currentTime),"%a, %d %b %Y %H:%M:%S %Z", &data);
+
+	switch(status_code)
+	{
+		case 400: snprintf(str, sizeof(str), "HTTP/1.1 400 Bad Request\r\nContent-Length: 95\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>400 Bad Request</TITLE></HEAD>\n<BODY><H1>400 Bad Rqeuest</H1>\n</BODY></HTML>", currentTime);
+				  printf("400 Bad Request\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 403: snprintf(str, sizeof(str), "HTTP/1.1 403 Forbidden\r\nContent-Length: 112\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>403 Forbidden</TITLE></HEAD>\n<BODY><H1>403 Forbidden</H1><br>Permission Denied\n</BODY></HTML>", currentTime);
+				  printf("403 Forbidden\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 404: snprintf(str, sizeof(str), "HTTP/1.1 404 Not Found\r\nContent-Length: 91\r\nContent-Type: text/html\r\nConnection: keep-alive\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Found</TITLE></HEAD>\n<BODY><H1>404 Not Found</H1>\n</BODY></HTML>", currentTime);
+				  printf("404 Not Found\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 500: snprintf(str, sizeof(str), "HTTP/1.1 500 Internal Server Error\r\nContent-Length: 115\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>500 Internal Server Error</TITLE></HEAD>\n<BODY><H1>500 Internal Server Error</H1>\n</BODY></HTML>", currentTime);
+				  //printf("500 Internal Server Error\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 501: snprintf(str, sizeof(str), "HTTP/1.1 501 Not Implemented\r\nContent-Length: 103\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>404 Not Implemented</TITLE></HEAD>\n<BODY><H1>501 Not Implemented</H1>\n</BODY></HTML>", currentTime);
+				  printf("501 Not Implemented\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		case 505: snprintf(str, sizeof(str), "HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 125\r\nConnection: keep-alive\r\nContent-Type: text/html\r\nDate: %s\r\nServer: VaibhavN/14785\r\n\r\n<HTML><HEAD><TITLE>505 HTTP Version Not Supported</TITLE></HEAD>\n<BODY><H1>505 HTTP Version Not Supported</H1>\n</BODY></HTML>", currentTime);
+				  printf("505 HTTP Version Not Supported\n");
+				  send(socket, str, strlen(str), 0);
+				  break;
+
+		default:  return -1;
+
+	}
+	return 1;
 }
--- include/functions.h
+++ include/functions.h
@@ -14,5 +14,10 @@
 
 handle_request(int client_socket, struct ParsedRequest *request, char* tempreq);
 connectRemoteServer(char* host_addrs, int server_port);
+int checkHTTPversion(char* version);
+int ErrorMessage(int socket, int status_code);
+int adding_element_in_cache(char* data, int len, char* url);
+struct element_inside_cache* find(char* url);
+void remove_cache_element();
 
 #endif
